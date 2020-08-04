fa46b38fa49a351c38ed7c3841538b7170512c4b
Erwee van Staden <erweevs@gmail.com>
2020-08-04 07:42:55 +0200

    another try at remvoing cached files

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import {render} from 'react-dom';
 
 render(
-    <div>Cryptochain in react! : live update</div>,
+    <div>Cryptochain in react! : live update 3</div>,
     document.getElementById('root')
 );
