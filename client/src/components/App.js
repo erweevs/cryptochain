a6e7df3190f7401642380c3b6a2a61fcf71ae3f8
Erwee van Staden <erweevs@gmail.com>
2020-08-04 08:02:44 +0200

    Added the App component Updated Notes with & vs &&

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+// declare the React component
+class App extends Component {
+    render(){
+        return(
+            <div>
+                Welcome to the Blockchain
+            </div>
+        );
+    }
+}
+
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import {render} from 'react-dom';
+import App from './components/App';
 
 render(
-    <div>Cryptochain in react! : live update 3</div>,
+    // render the App component
+    <App/>,
     document.getElementById('root')
 );
