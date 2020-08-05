3900a0dfe650c03042deba4ae63493772a132d1d
Erwee van Staden <erweevs@gmail.com>
2020-08-05 09:17:06 +0200

    Added Babel modules Fetch wallet-info from api end point Display wallet info on the front end

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,10 +2,26 @@ import React, {Component} from 'react';
 
 // declare the React component
 class App extends Component {
+    // add the sate
+    state = {
+        walletInfo:{ }
+    };
+
+    // this fires when the componenet has been inserted into the main document
+    componentDidMount(){
+        fetch('http://localhost:3000/api/wallet-info')
+        .then(response => response.json())
+        .then(json => this.setState({walletInfo: json}));
+    };
+
     render(){
+        const {address, balance} = this.state.walletInfo;
+
         return(
             <div>
-                Welcome to the Blockchain
+                <div>Welcome to the Blockchain</div>
+                <div>Address: {address}</div>
+                <div>Balance: {balance}</div>
             </div>
         );
     }
