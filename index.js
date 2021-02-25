0dbe526a39175f6b25a0a569e8f22d7847b64306
Erwee van Staden <erweevs@gmail.com>
2021-02-25 16:35:19 +0200

    Added hosted connection string, added env check

--- index.js
+++ index.js
@@ -12,6 +12,19 @@ const TransactionPool = require('./wallet/transaction-pool');
 const Wallet = require('./wallet');
 const TransactionMiner = require('./app/transaction-miner');
 
+const isDevelopment = process.env.ENV === 'development';
+
+// determine the Redis URL
+const REDIS_URL = isDevelopment ? 
+    'redis://127.0.0.1:6379' :
+    'blockchain-test.redis.cache.windows.net:6380,password=jfq7MSgybryb0E6c9u78mYmbfjuQm49D4DpG9z+5MQc=,ssl=True,abortConnect=False';
+
+    // Default port
+const DEFAULT_PORT = 3000;
+
+// Get the address of the root node
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
+
 // Create the local app
 const app = express();
 
@@ -19,19 +32,13 @@ const app = express();
 const blockchain = new Blockchain();
 const transactionPool = new TransactionPool();
 const wallet = new Wallet();
-const pubsub = new PubSub({blockchain, transactionPool});
+const pubsub = new PubSub({blockchain, transactionPool, redisUrl: REDIS_URL});
 const transactionMiner = new TransactionMiner({
     blockchain,
     transactionPool,
     wallet,
     pubsub
 });
-
-// Default port
-const DEFAULT_PORT = 3000;
-
-// Get the address of the root node
-const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 
 // Configure express to use the body-parser middleware
 app.use(bodyParser.json());
@@ -156,52 +163,53 @@ const syncWithRootState = () => {
 };
 
 // ====== seed the backend with dev data
-const walletFoo = new Wallet();
-const walletBar = new Wallet();
+if(isDevelopment){
+    const walletFoo = new Wallet();
+    const walletBar = new Wallet();
 
-const generateWalletTransaction = ({wallet, recipient, amount}) => {
-    const transaction = wallet.createTransaction({
-        recipient,
-        amount,
-        chain: blockchain.chain
+    const generateWalletTransaction = ({wallet, recipient, amount}) => {
+        const transaction = wallet.createTransaction({
+            recipient,
+            amount,
+            chain: blockchain.chain
+        });
+
+        transactionPool.setTransaction(transaction);
+    };
+
+    const walletAction = () => generateWalletTransaction({
+        wallet: wallet,
+        recipient: walletFoo.publicKey,
+        amount: 5
     });
 
-    transactionPool.setTransaction(transaction);
-};
+    const walletFooAction = () => generateWalletTransaction({
+        wallet: walletFoo,
+        recipient: walletBar.publicKey,
+        amount: 10
+    });
 
-const walletAction = () => generateWalletTransaction({
-    wallet: wallet,
-    recipient: walletFoo.publicKey,
-    amount: 5
-});
+    const walletBarAction = () => generateWalletTransaction({
+        wallet: walletBar,
+        recipient: wallet.publicKey,
+        amount: 15
+    });
 
-const walletFooAction = () => generateWalletTransaction({
-    wallet: walletFoo,
-    recipient: walletBar.publicKey,
-    amount: 10
-});
+    for(let i = 0; i < 10; i++){
+        if(i % 3 === 0){
+            walletAction();
+            walletFooAction();
+        } else if(i % 3 === 1){
+            walletAction();
+            walletBarAction();
+        }else{
+            walletFooAction();
+            walletBarAction();
+        }
 
-const walletBarAction = () => generateWalletTransaction({
-    wallet: walletBar,
-    recipient: wallet.publicKey,
-    amount: 15
-});
-
-for(let i = 0; i < 10; i++){
-    if(i % 3 === 0){
-        walletAction();
-        walletFooAction();
-    } else if(i % 3 === 1){
-        walletAction();
-        walletBarAction();
-    }else{
-        walletFooAction();
-        walletBarAction();
-    }
-
-    transactionMiner.mineTransaction();
+        transactionMiner.mineTransaction();
 }
-
+}
 // ========
 
 let PEER_PORT;
@@ -210,7 +218,7 @@ if(process.env.GENERATE_PEER_PORT === 'true'){
     PEER_PORT = DEFAULT_PORT + (Math.ceil(Math.random() * 1000));
 }
 
-const PORT = PEER_PORT || DEFAULT_PORT;
+const PORT = process.env.PORT || PEER_PORT || DEFAULT_PORT;
 
 // Tell the app to listen to incoming requests
 app.listen(PORT, () => {
